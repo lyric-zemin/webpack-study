ecbc9609fcf850f1417d432d1501b5c9eb41d1b3
wu.zeMin <244@qq.com>
2022-06-15 10:57:47 +0800

    test: 💍 CopyPlugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require("@vue/cli-service");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin("CopyPlugin").use(CopyPlugin, [
+      {
+        patterns: [
+          {
+            // 相对于当前文件路径
+            from: "./node_modules/bl",
+            // 相对于输出目录路径
+            to: "./bl",
+            filter: (resourcePath) => !(resourcePath.indexOf("package") > -1),
+          },
+          {
+            from: "./node_modules/csso/lib",
+            to: "./csso",
+          },
+        ],
+      },
+    ]);
+  },
 });
